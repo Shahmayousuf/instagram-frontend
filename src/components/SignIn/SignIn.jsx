749c96c60d03e190769c8296be993e3dec674d5b
Shahmayousuf <shahmayousuf@gmail.com>
2023-11-09 13:09:32 +0530

    add SignIn

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import './SignIn.css'
 const SignIn = () => {
   return (
-    <div>
-      Signin
-    </div>
-  )
-}
+    <div className="mycard">
+      <div className="card auth-card input-field">
+        <h2>Instagram</h2>
+        <input type="text" placeholder="email"></input>
+        <input type="text" placeholder="password"></input>
 
-export default SignIn
+        <button class="btn waves-effect waves-light #64b5f6 blue lighten-2">Login</button>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
