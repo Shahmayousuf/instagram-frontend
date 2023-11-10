0c76af2e3c24a2b5558b5d841093e6cd2a08bb2d
Shahmayousuf <shahmayousuf@gmail.com>
2023-11-10 12:39:44 +0530

    add links

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './SignIn.css'
+import {Link} from 'react-router-dom'
 const SignIn = () => {
   return (
     <div className="mycard">
@@ -7,8 +8,10 @@ const SignIn = () => {
         <h2>Instagram</h2>
         <input type="text" placeholder="email"></input>
         <input type="text" placeholder="password"></input>
-
         <button class="btn waves-effect waves-light #64b5f6 blue lighten-2">Login</button>
+        <h5>
+          <Link to={'/signup'}>dont have an account?</Link>
+        </h5>
       </div>
     </div>
   );
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import './SignUp.css'
+import {Link} from 'react-router-dom'
 const SignUp = () => {
   return (
-    <div>
-     <h1> SignUp</h1>
-    </div>
-  )
-}
+    <div className="mycard">
+      <div className="card auth-card input-field">
+        <h2>Instagram</h2>
+        <input type="text" placeholder="Name"></input>
+        <input type="text" placeholder="email"></input>
+        <input type="text" placeholder="password"></input>
 
-export default SignUp
+        <button class="btn waves-effect waves-light #64b5f6 blue lighten-2">SignUp</button>
+        <h5>
+          <Link to={'/signin'}>Already have an Account?</Link>
+        </h5>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
