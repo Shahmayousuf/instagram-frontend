ebc95c72f1694743404f8a54251d404384718476
Shahmayousuf <shahmayousuf@gmail.com>
2023-11-10 15:22:40 +0530

    add home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
-
+import './Home.css'
 const home = () => {
   return (
-    <div>
-      home
+    <div className='home'>
+      <div className='card home-card'>
+     
+      <h5>ramesh varma</h5>
+      <div className='card-image'>
+        <img src='https://i.pinimg.com/236x/51/44/71/5144713488ef4a7f88c98ebe34fff03a.jpg'></img>
+      </div>
+    
+      <div className='card-content'>
+      <i class="material-icons">favorite</i>
+        <h6>title</h6>
+        <p>this is a amazing post</p>
+        <input type='text'placeholder='add comment'></input>
+      </div>
+      </div>
+      <div className='card home-card'>
+    
+      <h5>ramesh varma</h5>
+      <div className='card-image'>
+        <img src='https://i.pinimg.com/236x/51/44/71/5144713488ef4a7f88c98ebe34fff03a.jpg'></img>
+      </div>
+      <div className='card-content'>
+      <i class="material-icons">favorite</i>
+        <h6>title</h6>
+        <p>this is a amazing post</p>
+        <input type='text'placeholder='add comment'></input>
+      </div>
+      </div>
+      <div className='card home-card'>
+    
+      <h5>ramesh varma</h5>
+      <div className='card-image'>
+        <img src='https://i.pinimg.com/236x/51/44/71/5144713488ef4a7f88c98ebe34fff03a.jpg'></img>
+      </div>
+      <div className='card-content'>
+      <i class="material-icons">favorite</i>
+        <h6>title</h6>
+        <p>this is a amazing post</p>
+        <input type='text'placeholder='add comment'></input>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,70 @@
-import React from 'react'
-
+import React from "react";
+import './profile.css'
+import profile from "../assets/profile.jpg";
+import pic from '../assets/pic.webp'
 const Profile = () => {
   return (
-    <div>
-      profile
-    </div>
-  )
-}
+    <div className="profile">
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          margin: "18px 0px",
+          borderBottom: "1px solid black",
+        }}
+      >
+        <div className="profile_icon">
+          <img
+            src={profile}
+            style={{ width: "160px", height: "160px", }}
+          ></img>
+        </div>
+        <div>
+          <h4>Ramesh Varma</h4>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              width: "108%",
+            }}
+          >
+            <h6>40 posts</h6>
+            <h6>45 followers</h6>
+            <h6>40 following</h6>
+          </div>
+        </div>
+      </div>
+      <div className="gallary">
+        <img className="item"
+          src={pic}
+          style={{ width: "160px", height: "160px", }}
+        ></img>
+                <img className="item"
 
-export default Profile
+          src={pic}
+          style={{ width: "160px", height: "160px", }}
+        ></img>
+        <img className="item"
+          src={pic}
+          style={{ width: "160px", height: "160px", }}
+        ></img>
+        <img className="item"
+
+          src={pic}
+          style={{ width: "160px", height: "160px", }}
+        ></img>
+        <img className="item"
+          src={pic}
+          style={{ width: "160px", height: "160px",  }}
+        ></img>
+         <img className="item"
+          src={pic}
+          style={{ width: "160px", height: "160px", }}
+        ></img>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
